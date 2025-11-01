e47cf5db0d9cd8a72fb23b9a2238d7d2a258888b
codeprnv <pranavpradhan23424@gmail.com>
2025-11-01 11:55:44 +0530

    First commit

---
+++ client/src/pages/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-screen text-2xl bg-red-400'>App</div>
+  )
+}
+
+export default App
